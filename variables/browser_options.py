411969fa9641bce2988499136d6ee7b61a850345
samsaysong <bobo,pro.dn@gmail.com>
2025-08-04 16:42:39 +0700

    hihi

--- variables/browser_options.py
+++ variables/browser_options.py
@@ -34,7 +34,8 @@ def get_chrome_options_object():
     # -------------------------------------------------------------
     
     # Kích hoạt chế độ headless MỚI (Chrome 109+) - Cần thiết cho headless
-    options.add_argument("--headless=new") 
+    # options.add_argument("--headless=new") 
+    options.add_argument("--incognito") 
     # options.add_argument("--guest") # Chế độ khách, không cần đăng nhập
     
     
